c9c96eab9783ec911fe872d32ec2ff357a95369e
miacias <miaciasullo@gmail.com>
2023-04-21 13:05:09 -0400

    saving changes for first deploy

---
+++ src/components/About/index.tsx
@@ -0,0 +1,16 @@
+import styles from './About.module.css';
+
+export default function About() {
+    return (
+        <>
+            <h2>About Me</h2>
+            <p>I am a former French teacher turned full-stack developer. As someone who has always been drawn to the creative and artistic, I have found programming to be the perfect outlet for my unique perspective and problem-solving skills.
+
+My journey into the world of tech began when I discovered my passion for designing and building websites. The more I learned, the more fascinated I became with the endless possibilities of coding. From crafting intuitive user interfaces to optimizing backend functionality, I relish the opportunity to build innovative and efficient solutions.
+
+As a former teacher, I bring a collaborative approach to my work, always striving to understand the needs and goals of my clients and team members. Whether I'm building a custom web application or optimizing existing code, I am committed to delivering quality results that exceed expectations.
+
+Thank you for taking the time to learn more about me and my work. Please feel free to browse my portfolio and contact me if you have any questions or if you're interested in working together.</p>
+        </>
+    )
+}
