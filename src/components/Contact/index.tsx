bb447a42d4b8b0ee2c76e6cc6a30d270c261606e
miacias <miaciasullo@gmail.com>
2023-04-23 19:32:55 -0400

    basic form with Final Form library is built. need to add send email feature

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -17,13 +17,12 @@ export default function Contact() {
         console.log(event)
         const errors: errors = {};
         // email regex https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
-        if (event.email && event.email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)) {
-            errors.email = 'please fix email'
+        if (event.email && !event.email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)) {
+            errors.email = 'please complete email';
         }
         if (event.message && event.message.length < 5) {
-            errors.message = 'message too short'
+            errors.message = 'message too short';
         }
-        console.log(errors)
         return errors;
     };
     return (
@@ -33,13 +32,30 @@ export default function Contact() {
             validate={validate} 
             render={( {handleSubmit} ) => (
                 <form onSubmit={handleSubmit}>
-                    <div className={styles.field}>
+                    {/* <div className={styles.field}>
                         <label>email</label>
-                        <Field name='email' component='input' placeholder='email' />
+                        <Field 
+                            name='email' 
+                            component='input' 
+                            placeholder='email'
+                        />
+                    </div> */}
+                    <div className={styles.field}>
+                        <Field
+                            name='email' 
+                            render={({input, meta}) => (
+                                <div>
+                                    <label>email</label>
+                                    <textarea {...input} />
+                                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                                </div>
+                            )}
+                        />
                     </div>
                     <div className={styles.field}>
                         <Field
-                            name='message' render={({input, meta}) => (
+                            name='message' 
+                            render={({input, meta}) => (
                                 <div>
                                     <label>message</label>
                                     <textarea {...input} />
