334092a5417d17f4b177764058c38b35af74b1a8
miacias <miaciasullo@gmail.com>
2023-04-23 21:05:39 -0400

    emailjs cannot be tested b/c typescript needs to be resolved first. error: TS2307: Cannot find module \'@emailjs/browser\' or its corresponding type declarations.

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Form, Field } from 'react-final-form'
+import emailjs from '@emailjs/browser';
 import styles from './Contact.module.css';
 
 type errors = {
+    name?: string;
     email?: string;
     message?: string;
 }
 
 export default function Contact() {
-    const onSubmit = (event: EventTarget) => {
-        debugger
+
+    const onSubmit = (event: any) => {
+        // event.preventDefault()
+        emailjs.sendForm('service_tjsoedn', 'template_ly5vrzm', '.contact-form', 'DOsrgnZ-5O0QZj-fu')
+            .then((response: any) => {
+                console.log(response.text);
+            }, (error: any) => {
+                console.log(error.text);
+            });
+            event.target.reset();
     };
     // disables form Submit if there is an error
     const validate = (event: any): any => {
         console.log(event)
         const errors: errors = {};
+        if (event.name && event.name.length < 2) {
+            errors.name = 'missing name'
+        }
         // email regex https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
         if (event.email && !event.email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)) {
             errors.email = 'please complete email';
@@ -28,6 +41,7 @@ export default function Contact() {
     return (
     <>
         <Form 
+            className='contact-form'
             onSubmit={onSubmit} 
             validate={validate} 
             render={( {handleSubmit} ) => (
@@ -40,6 +54,18 @@ export default function Contact() {
                             placeholder='email'
                         />
                     </div> */}
+                    <div className={styles.field}>
+                        <Field
+                            name='name' 
+                            render={({input, meta}) => (
+                                <div>
+                                    <label>name</label>
+                                    <textarea {...input} />
+                                    {meta.touched && meta.error && <span>{meta.error}</span>}
+                                </div>
+                            )}
+                        />
+                    </div>
                     <div className={styles.field}>
                         <Field
                             name='email' 
